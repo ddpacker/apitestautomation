82411bf2255987592ec193767e11214332d66983
Darren Packer <darren.d.packer@gmail.com>
2019-07-11 17:46:57 -0400

    Modified upload service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadService } from './services/upload.service';
 
 import { ApiDocManagerModule } from './modules/api-doc-manager/api-doc-manager.module'
 
@@ -13,7 +15,8 @@ import { ApiDocManagerModule } from './modules/api-doc-manager/api-doc-manager.m
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ApiDocManagerModule
+    ApiDocManagerModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/api-doc-manager/components/api-doc/api-doc.component.ts
+++ src/app/modules/api-doc-manager/components/api-doc/api-doc.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output, Input, ViewChild } from '@angular/core';
 import apiUpload from '../../../../models/apiUpload'
+import { UploadService } from '../../../../services/upload.service'
 
 @Component({
   selector: 'app-api-doc',
@@ -11,7 +12,23 @@ export class ApiDocComponent implements OnInit {
   @Input() apiUpload: apiUpload;
   @Output() sendEvent = new EventEmitter();
 
+  fileToUpload: File = null;
+  uploadService: UploadService;
+
+  handleUpload(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+
+  uploadFile() {
+    this.uploadService.post(this.fileToUpload).subscribe(data => {
+      console.log("Success!")
+    }, error => {
+      console.log(error);
+    });
+  }
+
   addApi(path: string, api: string) {
+    this.uploadFile();
     const newApi = new apiUpload(null, path, api);
     this.sendEvent.emit(["add", newApi])
   }
